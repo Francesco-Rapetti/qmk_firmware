6e8a12a950e9356e4ca4f08d05b2ef93732948b1
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-03 19:07:20 +0200

    Layer led turn white if fn

--- keyboards/nuphy/air75_v2/ansi/ansi.c
+++ keyboards/nuphy/air75_v2/ansi/ansi.c
@@ -706,11 +706,17 @@ bool rgb_matrix_indicators_user(void)
     rgb_matrix_set_color(RGB_MATRIX_LED_COUNT-1, 0, 0, 0);
 
     switch(get_highest_layer(layer_state|default_layer_state)) {
+            case 0:
+                rgb_matrix_set_color(73, 0, 0, 0);
+                break;
+            case 1:
+                rgb_matrix_set_color(73, RGB_WHITE);
+                break;
             case 2:
-                rgb_matrix_set_color(73, RGB_BLUE);
+                rgb_matrix_set_color(73, 0, 0, 0);
                 break;
             case 3:
-                rgb_matrix_set_color(73, RGB_YELLOW);
+                rgb_matrix_set_color(73, RGB_WHITE);
                 break;
             default:
                 break;
